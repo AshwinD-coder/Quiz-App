7dd29a7a173826e2fafae2d2be76365c54917fb3
Dipendra Tamang <dipendra.tamang9@gmail.com>
2023-10-31 21:43:50 +0545

    change application properties to application.yaml

--- src/main/java/com/quizmasterdeluxe/resource/LandingPageController.java
+++ src/main/java/com/quizmasterdeluxe/resource/LandingPageController.java
@@ -1,18 +1,9 @@
 package com.quizmasterdeluxe.resource;
 
-import com.github.jknack.handlebars.Context;
-import com.github.jknack.handlebars.Handlebars;
-import com.github.jknack.handlebars.Template;
-import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
-import com.github.jknack.handlebars.io.TemplateLoader;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.views.View;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller("/")
 public class LandingPageController {

1ededeb171a6392b8019fffac68c05a341ea4afd
Dipendra Tamang <dipendra.tamang9@gmail.com>
2023-11-02 10:51:35 +0545

    adding constructor on custom exception

--- src/main/java/com/quizmasterdeluxe/controller/LandingPageController.java
+++ src/main/java/com/quizmasterdeluxe/controller/LandingPageController.java
@@ -10,7 +10,7 @@ import io.micronaut.views.View;
 public class LandingPageController {
     @View()
     @Get()
-    public ModelAndView index()  {
+    public ModelAndView renderingLandingPage()  {
         return  new ModelAndView("landing-page",null);
     }
 }
--- src/main/java/com/quizmasterdeluxe/platform/exception/QuizMasterException.java
+++ src/main/java/com/quizmasterdeluxe/platform/exception/QuizMasterException.java
@@ -14,6 +14,7 @@ public class QuizMasterException extends RuntimeException{
 
     public QuizMasterException(QuizMasterExceptionType type) {
         super(type.getMessage());
+        this.type=type;
 
     }
 }
